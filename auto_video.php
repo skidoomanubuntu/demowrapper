86627e19ab4ddc5f56414a184b1a8d3ddcb41bcb
Steve Barriault <shidifu05@gmail.com>
2022-12-08 13:53:51 -0500

    In case this is the problem

---
+++ auto_video.php
@@ -0,0 +1,19 @@
+<html>
+<style>
+body, html {
+    min-height: 100%;
+    min-width: 100%;
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+}
+</style>
+
+<body width="100%" height="100%">
+  <video width="100%" height="100%" autoplay loop>
+    <source src="auto_video.mp4" type="video/mp4">
+    <!-- <source src="movie.ogg" type="video/ogg"> -->
+  Your browser does not support the video tag.
+  </video>
+</body>
+</html>
